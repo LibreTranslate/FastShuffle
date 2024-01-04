7581f9f414e5f4ed36e067d0ee6f298252e5afe9
Piero Toffanin <pt@masseranolabs.com>
2024-01-03 19:02:00 -0500

    Fix test

--- tests/test_shuffle.py
+++ tests/test_shuffle.py
@@ -5,12 +5,14 @@ import os
 def test_shuffle():
     cwd = os.path.dirname(__file__)
 
-    src, tgt = file_shuffle(os.path.join(cwd, "data", "src.txt"), os.path.join(cwd, "data", "tgt.txt"))
+    in_src = os.path.join(cwd, "data", "src.txt")
+    in_tgt = os.path.join(cwd, "data", "tgt.txt")
+    src, tgt = file_shuffle(in_src, in_tgt)
 
     assert os.path.isfile(src)
     assert os.path.isfile(tgt)
 
-    assert sum(1 for _ in open(src)) == 5
-    assert sum(1 for _ in open(tgt)) == 5
+    assert sum(1 for _ in open(src)) == sum(1 for _ in open(in_src))
+    assert sum(1 for _ in open(tgt)) == sum(1 for _ in open(in_tgt))
     
     
